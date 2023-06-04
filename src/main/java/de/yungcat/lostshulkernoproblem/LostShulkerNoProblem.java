34aa6360b8649fe71c0a8965808053b8aa6a18ea
Andreas | YungCat <koalaandi4@gmail.com>
2023-06-04 13:19:17 +0200

    Shulker Data now gets saved into a JSON File using GSON

--- src/main/java/de/yungcat/lostshulkernoproblem/LostShulkerNoProblem.java
+++ src/main/java/de/yungcat/lostshulkernoproblem/LostShulkerNoProblem.java
@@ -1,17 +1,39 @@
 package de.yungcat.lostshulkernoproblem;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.bukkit.plugin.java.JavaPlugin;
 import de.yungcat.lostshulkernoproblem.handler.EventProcessor;
 
+import java.io.File;
+import java.io.IOException;
+
 public final class LostShulkerNoProblem extends JavaPlugin {
+
+    public Gson gson;
+    public File dataFile;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
         new EventProcessor(this);
-    }
 
+        gson = new GsonBuilder().setPrettyPrinting().create();
+
+        File dataFolder = getDataFolder();
+        dataFile = new File(dataFolder, "lostShulker.json");
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
+        }
+        else if (!dataFile.exists()) {
+            try {
+                dataFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
     @Override
     public void onDisable() {
         // Plugin shutdown logic
--- src/main/java/de/yungcat/lostshulkernoproblem/handler/EventProcessor.java
+++ src/main/java/de/yungcat/lostshulkernoproblem/handler/EventProcessor.java
@@ -1,5 +1,6 @@
 package de.yungcat.lostshulkernoproblem.handler;
 
+import com.google.gson.JsonObject;
 import de.yungcat.lostshulkernoproblem.LostShulkerNoProblem;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -12,6 +13,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class EventProcessor implements Listener {
 
@@ -30,6 +36,8 @@ public class EventProcessor implements Listener {
     //Player Data
     Player playerPlaced;
     String playerName;
+    UUID playerUUID;
+    UUID worldUUID;
 
     //Shulker Position
     Location blockPos;
@@ -38,6 +46,10 @@ public class EventProcessor implements Listener {
     int blockPosZ;
     String blockPosWorld;
     String blockPosXYZ;
+    String blockTypeString;
+
+    //List with all the necessary data to save
+    String[] shulkerData;
 
     //Saving
 
@@ -48,6 +60,10 @@ public class EventProcessor implements Listener {
         blockState = e.getBlockPlaced().getState();
         playerPlaced = e.getPlayer();
         playerName = e.getPlayer().getName();
+        playerUUID = playerPlaced.getUniqueId();
+        worldUUID = blockPlaced.getWorld().getUID();
+
+        String shulkerData;
 
         if (blockState instanceof ShulkerBox) {
             //Set all Variables
@@ -59,27 +75,124 @@ public class EventProcessor implements Listener {
             blockPosXYZ = "x: " + blockPosX + ", y: " + blockPosY + ", z: " + blockPosZ;
 
             //Changing World Name to make it more readable for user
-            if(blockPos.getWorld().getName().equals("world")) {
+            /*if(blockPos.getWorld().getName().equals("world")) {
                 blockPosWorld = "Overworld";
             }
             else if (blockPos.getWorld().getName().equals("world_nether")) {
                 blockPosWorld = "Nether";
             }
             else blockPosWorld = "The End";
+            */
+            //DATA Structure
+            Map<String, Map<String, List<Coordinates>>> jsonData = new HashMap<>();
+
+            //List for the Coordinates
+            List<Coordinates> coordinates = new ArrayList<>();
+            coordinates.add(new Coordinates(worldUUID, blockPosX, blockPosY, blockPosZ));
+
+            //Map Entry for the Shulker Colors
+            Map<String, List<Coordinates>> shulkerColors = new HashMap<>();
+            shulkerColors.put(blockType.toString(), coordinates);
+
+            //Map Entry for the Player UUID
+            Map<String, Map<String, List<Coordinates>>> playerData = new HashMap<>();
+            playerData.put(playerUUID.toString(), shulkerColors);
+
+            jsonData.putAll(playerData);
+            shulkerData = jsonData.toString();
+            Bukkit.getConsoleSender().sendMessage(shulkerData);
 
             //Changing blockType Name to make it more readable
-            String blockTypeString;
             blockTypeString = blockType.toString().replace("_", " ");
             blockTypeString = blockTypeString.toLowerCase();
 
+            //Setting all variables to the String[]
+            //shulkerData = new String[]{playerName, blockTypeString, blockPosXYZ, blockPosWorld};
+
             //DEBUG PLAYER MESSAGE
-            playerPlaced.sendMessage(playerName + "\n" + blockTypeString + "\n" + blockPosXYZ + "\n" + blockPosWorld);
+            //playerPlaced.sendMessage(playerName + "\n" + blockTypeString + "\n" + blockPosXYZ + "\n" + blockPosWorld);
+
+            //Saving the Data into the JSON File
+            String uuid = plugin.gson.toJson(jsonData);
+            try (FileWriter writer = new FileWriter(plugin.dataFile, true)) {
+                writer.write(uuid);
+                writer.write(System.lineSeparator());
+            } catch (IOException excep) {
+                excep.printStackTrace();
+            }
         }
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent e) {
+        Block brokenBlock = e.getBlock();
+        BlockState brokenBlockState = e.getBlock().getState();
+        UUID playerBrokeBlock = e.getPlayer().getUniqueId();
+        Material brokenBlockType = brokenBlock.getType();
+        UUID worldBrokenBlock = brokenBlock.getWorld().getUID();
+        int brokenPosX = brokenBlock.getX();
+        int brokenPosY = brokenBlock.getY();
+        int brokenPosZ = brokenBlock.getZ();
+
+        if (brokenBlockState instanceof ShulkerBox) {
+
+            //DATA Structure
+            Map<String, Map<String, List<Coordinates>>> brokenShulkerData = new HashMap<>();
+
+            //List for the Coordinates
+            List<Coordinates> brokenShulkerCoordinates = new ArrayList<>();
+            brokenShulkerCoordinates.add(new Coordinates(worldUUID, blockPosX, blockPosY, blockPosZ));
+
+            //Map Entry for the Shulker Colors
+            Map<String, List<Coordinates>> shulkerColors = new HashMap<>();
+            shulkerColors.put(blockType.toString(), brokenShulkerCoordinates);
+
+            //Map Entry for the Player UUID
+            Map<String, Map<String, List<Coordinates>>> playerData = new HashMap<>();
+            playerData.put(playerUUID.toString(), shulkerColors);
+
+            brokenShulkerData.putAll(playerData);
+            //shulkerData = brokenShulkerData.toString();
+            Bukkit.getConsoleSender().sendMessage(shulkerData);
+        }
+    }
+
+
+    public void removeFieldsIfSame() {
+        JsonObject jsonObject = readShulkerData();
 
     }
-    //customName of Shulker Box in Array
+
+    private JsonObject readShulkerData() {
+        JsonObject jsonObject = new JsonObject();
+
+        try (FileReader reader = new FileReader(plugin.dataFile)) {
+            jsonObject = plugin.gson.fromJson(reader, JsonObject.class);
+        } catch (IOException excep) {
+            excep.printStackTrace();
+        }
+
+        return jsonObject;
+    }
+
+
+    //Coordinates Object for the adding and removing of the data in the json
+    class Coordinates {
+        private UUID world = worldUUID;
+        private int x = blockPosX;
+        private int y = blockPosY;
+        private int z = blockPosZ;
+
+        public Coordinates(UUID world, int x, int y, int z) {
+            this.world = world;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public UUID getWorld() {return world;}
+        public int x() {return x;}
+        public int y() {return y;}
+        public int z() {return z;}
+    }
 }
