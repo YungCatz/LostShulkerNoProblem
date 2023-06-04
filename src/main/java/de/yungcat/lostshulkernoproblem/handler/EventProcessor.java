a1ba371f8d2dd272601fd4410b521a041e747f62
Andreas | YungCat <koalaandi4@gmail.com>
2023-06-04 15:02:39 +0200

    Cleaned

--- src/main/java/de/yungcat/lostshulkernoproblem/handler/EventProcessor.java
+++ src/main/java/de/yungcat/lostshulkernoproblem/handler/EventProcessor.java
@@ -1,6 +1,5 @@
 package de.yungcat.lostshulkernoproblem.handler;
 
-import com.google.gson.JsonObject;
 import de.yungcat.lostshulkernoproblem.LostShulkerNoProblem;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -14,7 +13,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
@@ -28,61 +26,23 @@ public class EventProcessor implements Listener {
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
-    //Block Data
-    Block blockPlaced;
-    BlockState blockState;
-    Material blockType;
-
-    //Player Data
-    Player playerPlaced;
-    String playerName;
-    UUID playerUUID;
-    UUID worldUUID;
-
-    //Shulker Position
-    Location blockPos;
-    int blockPosX;
-    int blockPosY;
-    int blockPosZ;
-    String blockPosWorld;
-    String blockPosXYZ;
-    String blockTypeString;
-
-    //List with all the necessary data to save
-    String[] shulkerData;
-
-    //Saving
-
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent e) {
 
-        blockPlaced = e.getBlockPlaced();
-        blockState = e.getBlockPlaced().getState();
-        playerPlaced = e.getPlayer();
-        playerName = e.getPlayer().getName();
-        playerUUID = playerPlaced.getUniqueId();
-        worldUUID = blockPlaced.getWorld().getUID();
-
-        String shulkerData;
+        Block blockPlaced = e.getBlockPlaced();
+        BlockState blockState = e.getBlockPlaced().getState();
+        Player playerPlaced = e.getPlayer();
+        UUID playerUUID = playerPlaced.getUniqueId();
+        UUID worldUUID = blockPlaced.getWorld().getUID();
 
         if (blockState instanceof ShulkerBox) {
             //Set all Variables
-            blockType = blockPlaced.getType();
-            blockPos = blockPlaced.getLocation();
-            blockPosX = blockPlaced.getLocation().getBlockX();
-            blockPosY = blockPlaced.getLocation().getBlockY();
-            blockPosZ = blockPlaced.getLocation().getBlockZ();
-            blockPosXYZ = "x: " + blockPosX + ", y: " + blockPosY + ", z: " + blockPosZ;
-
-            //Changing World Name to make it more readable for user
-            /*if(blockPos.getWorld().getName().equals("world")) {
-                blockPosWorld = "Overworld";
-            }
-            else if (blockPos.getWorld().getName().equals("world_nether")) {
-                blockPosWorld = "Nether";
-            }
-            else blockPosWorld = "The End";
-            */
+            Material blockType = blockPlaced.getType();
+            Location blockPos = blockPlaced.getLocation();
+            int blockPosX = blockPlaced.getLocation().getBlockX();
+            int blockPosY = blockPlaced.getLocation().getBlockY();
+            int blockPosZ = blockPlaced.getLocation().getBlockZ();
+            
             //DATA Structure
             Map<String, Map<String, List<Coordinates>>> jsonData = new HashMap<>();
 
@@ -99,18 +59,6 @@ public class EventProcessor implements Listener {
             playerData.put(playerUUID.toString(), shulkerColors);
 
             jsonData.putAll(playerData);
-            shulkerData = jsonData.toString();
-            Bukkit.getConsoleSender().sendMessage(shulkerData);
-
-            //Changing blockType Name to make it more readable
-            blockTypeString = blockType.toString().replace("_", " ");
-            blockTypeString = blockTypeString.toLowerCase();
-
-            //Setting all variables to the String[]
-            //shulkerData = new String[]{playerName, blockTypeString, blockPosXYZ, blockPosWorld};
-
-            //DEBUG PLAYER MESSAGE
-            //playerPlaced.sendMessage(playerName + "\n" + blockTypeString + "\n" + blockPosXYZ + "\n" + blockPosWorld);
 
             //Saving the Data into the JSON File
             String uuid = plugin.gson.toJson(jsonData);
@@ -120,6 +68,9 @@ public class EventProcessor implements Listener {
             } catch (IOException excep) {
                 excep.printStackTrace();
             }
+
+            //DEBUG PLAYER MESSAGE
+            //playerPlaced.sendMessage(playerName + "\n" + blockTypeString + "\n" + blockPosXYZ + "\n" + blockPosWorld);
         }
     }
 
@@ -141,47 +92,26 @@ public class EventProcessor implements Listener {
 
             //List for the Coordinates
             List<Coordinates> brokenShulkerCoordinates = new ArrayList<>();
-            brokenShulkerCoordinates.add(new Coordinates(worldUUID, blockPosX, blockPosY, blockPosZ));
+            brokenShulkerCoordinates.add(new Coordinates(worldBrokenBlock, brokenPosX, brokenPosY, brokenPosZ));
 
             //Map Entry for the Shulker Colors
             Map<String, List<Coordinates>> shulkerColors = new HashMap<>();
-            shulkerColors.put(blockType.toString(), brokenShulkerCoordinates);
+            shulkerColors.put(brokenBlockType.toString(), brokenShulkerCoordinates);
 
             //Map Entry for the Player UUID
             Map<String, Map<String, List<Coordinates>>> playerData = new HashMap<>();
-            playerData.put(playerUUID.toString(), shulkerColors);
+            playerData.put(playerBrokeBlock.toString(), shulkerColors);
 
             brokenShulkerData.putAll(playerData);
-            //shulkerData = brokenShulkerData.toString();
-            Bukkit.getConsoleSender().sendMessage(shulkerData);
         }
     }
-
-
-    public void removeFieldsIfSame() {
-        JsonObject jsonObject = readShulkerData();
-
-    }
-
-    private JsonObject readShulkerData() {
-        JsonObject jsonObject = new JsonObject();
-
-        try (FileReader reader = new FileReader(plugin.dataFile)) {
-            jsonObject = plugin.gson.fromJson(reader, JsonObject.class);
-        } catch (IOException excep) {
-            excep.printStackTrace();
-        }
-
-        return jsonObject;
-    }
-
 
     //Coordinates Object for the adding and removing of the data in the json
-    class Coordinates {
-        private UUID world = worldUUID;
-        private int x = blockPosX;
-        private int y = blockPosY;
-        private int z = blockPosZ;
+    static class Coordinates {
+        private UUID world;
+        private int x;
+        private int y;
+        private int z;
 
         public Coordinates(UUID world, int x, int y, int z) {
             this.world = world;
